7956c3c78e99982531d80840f5cf52c79455e54c
iismail1997 <2015.ismail@gmail.com>
2018-06-27 13:35:09 -0500

    Added the challenge prompr for fibonacci seq.

--- learn_algo/src/algo/algo/Recursion/RecursionApp.java
+++ learn_algo/src/algo/algo/Recursion/RecursionApp.java
@@ -51,6 +51,15 @@ public class RecursionApp {
 
     // return the nth element in the sequence
     // ex: 0, 1, 1, 2, 3, 5, 8, 13, 21
+    /*
+    The fibonacci sequence is a famous bit of mathematics,
+    and it happens to have a recursive definition.
+    The first two values in the sequence are 0 and 1 (essentially 2 base cases).
+    Each subsequent value is the sum of the previous two values,
+    so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on.
+    Define a recursive fibonacci(n) method that returns the nth fibonacci number,
+    with n=0 representing the start of the sequence.
+     */
     public static int fibonacciSequence(int n){
         if (n == 0)
             return 0;
@@ -59,4 +68,5 @@ public class RecursionApp {
         else
             return (fibonacciSequence((n-1)) + fibonacciSequence(n-2));
     }
+
 }
