d01dfd76255da8a56c85ed5471509f005d12b2f9
iismail1997 <2015.ismail@gmail.com>
2018-07-26 00:43:28 -0500

    merge sort

--- learn_algo/src/algo/MergeSort/MergeSort.java
+++ learn_algo/src/algo/MergeSort/MergeSort.java
@@ -58,6 +58,54 @@ public class MergeSort {
             start++;
         }
 
+    }
+
+    // Another solution
+    public static void merge2(int inputArray[], int start, int mid, int end){
+        int tempArray[] = new int [end - start + 1];
+
+        // index coutner for the left side of the array
+        int leftSlot = start;
+        // index coutner for the right side of the array
+        int rightSlot = mid+1;
+        int k = 0;
+
+        while (leftSlot <= mid && rightSlot <= end){
+            if(inputArray[leftSlot] < inputArray[rightSlot]){
+                tempArray[k] = inputArray[leftSlot];
+                leftSlot++;
+            }
+            else {
+                tempArray[k] = inputArray[rightSlot];
+                rightSlot++;
+            }
+            k++;
+        }
+
+        /*
+         * WHen it gets to here, that neabs tge above loop is completed.
+         * So both the right and the left side of the sub-array can be considered sorted
+         */
+
+        if(leftSlot <= mid){ // consider the right side done being sorted. Left msut be sorted already
+            while (leftSlot <= mid){
+                tempArray[k] = inputArray[leftSlot];
+                leftSlot++;
+                k++;
+            }
+        }
+        else if( rightSlot <= end){
+            while (rightSlot <= end){
+                tempArray[k] = inputArray[rightSlot];
+                rightSlot++;
+                k++;
+            }
+        }
+
+        // copy over the temp array into the appropriate slot of the inputArray
+        for(int i = 0; i < tempArray.length; i++){
+            inputArray[start+i] = tempArray[i];
+        }
 
 
     }
